d79c4a5a7ab720e7f6fc5ea4208553b4a548f71e
Dan Taeyoung <provolot@gmail.com>
2021-01-21 12:17:17 -0500

    quick note

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="nav">
+    <p>We are working on a report about financially sustainable cooperative art/design schools. This report is being created for the Center for Cultural Innovation’s AmbitioUS program area, created to “encourage the development of burgeoning alternative economies and fresh social contracts in ways that artists and cultural communities can achieve financial freedom.”</p>
     <a href="https://airtable.com/tblShbhbI1OXPCNm4/viw7J9Aa5DbKIpx3K?blocks=hide">Airtable</a>
     <router-link :to="{ name: 'PatternsView' }">Patterns</router-link>
     <router-link :to="{ name: 'GroupsView' }">Groups</router-link>
@@ -27,6 +28,7 @@ export default {
   flex-direction: column;
   margin: 20px;
   border: 1px solid black;
+  padding: 10px;
 }
 
 </style>
